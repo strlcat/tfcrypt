427cccc02bf41be626fb70408e172d41fb1176b4
Andrey Rys <rys@lynxlynx.ru>
2022-08-28 12:32:18 +0200

    Sync with tfcipher

--- tfdef.h
+++ tfdef.h
@@ -92,6 +92,18 @@ static inline void data_to_words(void *p, size_t l)
 #endif
 }
 
+static inline void xor_block(void *dst, const void *src, size_t sz)
+{
+	const size_t *sx = (const size_t *)src;
+	const TF_BYTE_TYPE *usx = (const TF_BYTE_TYPE *)src;
+	size_t *dx = (size_t *)dst;
+	TF_BYTE_TYPE *udx = (TF_BYTE_TYPE *)dst;
+	size_t sl = sz;
+
+	for (sl = 0; sl < (sz / sizeof(size_t)); sl++) dx[sl] ^= sx[sl];
+	if (sz - (sl * sizeof(size_t))) for (sl *= sizeof(size_t); sl < sz; sl++) udx[sl] ^= usx[sl];
+}
+
 static inline void ctr_inc(TF_UNIT_TYPE *x, size_t xl)
 {
 	size_t z;
--- tfstream.c
+++ tfstream.c
@@ -2,18 +2,6 @@
 #include "tfdef.h"
 #include "tfe.h"
 
-static inline void xor_block(void *dst, const void *src, size_t sz)
-{
-	const size_t *sx = (const size_t *)src;
-	const TF_BYTE_TYPE *usx = (const TF_BYTE_TYPE *)src;
-	size_t *dx = (size_t *)dst;
-	TF_BYTE_TYPE *udx = (TF_BYTE_TYPE *)dst;
-	size_t sl = sz;
-
-	for (sl = 0; sl < (sz / sizeof(size_t)); sl++) dx[sl] ^= sx[sl];
-	if (sz - (sl * sizeof(size_t))) for (sl *= sizeof(size_t); sl < sz; sl++) udx[sl] ^= usx[sl];
-}
-
 void tf_stream_crypt(struct tfe_stream *tfe, void *out, const void *in, size_t sz)
 {
 	tfe_emit(out, sz, tfe);
